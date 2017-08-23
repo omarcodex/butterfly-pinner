8e585eddd7d6aff71ac54e2d049bdbe95deb56c3
omarcodex <omaralimalik@gmail.com>
2017-08-23 12:24:19 -0700

    Create application skeleton with React, Firebase

---
+++ src/javascripts/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+let database;
+export const init = () => {
+  let config = {
+    apiKey: 'AIzaSyBhSdMEX0P-QGwfZLOYLApr63rmERuxb_o',
+    authDomain: 'butterflypinner.firebaseapp.com',
+    databaseURL: 'https://butterflypinner.firebaseio.com',
+    projectId: 'butterflypinner',
+    storageBucket: 'butterflypinner.appspot.com',
+    messagingSenderId: '1000022264881'
+  };
+  firebase.initializeApp(config);
+  database = firebase.database();
+};
