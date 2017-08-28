785091a90b0d722f250c6fb3bb43ace27ee55b75
monkypuzzle <paul.newsam@gmail.com>
2017-08-27 21:41:39 -0700

    Adds GuideRecord component and associated module references

---
+++ src/components/GuideRecord.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Card, CardMedia, CardTitle, CardText } from "material-ui/Card";
+
+const GuideRecord = (props) => {
+  console.log(props)
+  return(
+    <Card>
+      <CardMedia overlay={<CardTitle title={props.scientificName} subtitle={props.familyName} />}>
+        <img src={props.photoUrl} alt={props.scientificName} />
+      </CardMedia>
+      <CardText>
+        {props.description}
+      </CardText>
+    </Card>
+  )
+}
+
+export default GuideRecord;
--- src/components/GuideWrapper.jsx
+++ src/components/GuideWrapper.jsx
@@ -1,20 +1,54 @@
 import React, { Component } from "react";
 import { Card, CardText, CardTitle } from "material-ui/Card";
 
+import GuideRecord from "./GuideRecord";
+
 class GuideWrapper extends Component {
-  // constructor(props){
-  //   super(props);
-  // }
+  constructor(props){
+    super(props);
+    this.state = {
+      data: [
+        {
+          id: 1,
+          scientificName: "Papilio eurymedon",
+          familyName: "Papilionidae",
+          description: "The Pale Swallowtail is a pale swallowtail.",
+          photoUrl: "/images/pale-swallowtail.jpg"
+        },
+        {
+          id: 2,
+          scientificName: "Danaus plexippus",
+          familyName: "Nymphalidae",
+          description: "The Monarch is a member of the Milkweed family famed for its multi-generational migrations.",
+          photoUrl: "./images/monarch.jpg"
+        },
+        {
+          id: 3,
+          scientificName: "Nymphalis antiopa",
+          familyName: "Nymphalidae",
+          description: "The Mourning Cloak is a large brown butterfly with yellow-fringes on its wings.",
+          photoUrl: "/images/mourning-cloak.jpg"
+        }
+      ]
+    }
+  }
 
   render(){
-    return(
-      <Card className="guide__card">
-        <CardTitle
-        title="Butterfly Guide"
+    let records = this.state.data.map((record)=>{
+      return(
+        <GuideRecord
+          scientificName={record.scientificName}
+          familyName={record.familyName}
+          description={record.description}
+          photoUrl={record.photoUrl}
+          key={record.id}
         />
-        <CardText>
-        </CardText>
-      </Card>
+      )});
+    return(
+      <div>
+        <h1>Butterfly Guide</h1>
+        {records}
+      </div>
     )
   }
 }
