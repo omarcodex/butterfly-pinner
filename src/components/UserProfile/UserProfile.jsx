0af34fe3a15211e96e0a1200069c17c53ba84f5d
monkypuzzle <paul.newsam@gmail.com>
2017-08-30 20:04:23 -0700

    Styles Profile page

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,18 +1,38 @@
 import React from "react";
 import { Card, CardTitle, CardText } from "material-ui/Card";
+import Avatar from "material-ui/Avatar";
+import "./UserProfile.css";
 
 const UserProfile = (props) => {
   console.log(props)
-  let name;
-  if (props.currentUser) { name = props.currentUser.displayName };
+  let name, photoUrl, email;
+  if (props.currentUser) {
+    name = props.currentUser.displayName;
+    photoUrl = props.currentUser.photoURL;
+    email = props.currentUser.email;
+  };
   return(
-    <Card>
-      <CardTitle title="Profile"/>
-      <CardText>
-        <span>Name: {name}</span>
+    <Card className="user-profile__container">
+      <CardTitle title={name + "'s Profile"}/>
+      <CardText className="row">
+      <div className="one-third column">
+        <Avatar
+        src={photoUrl}
+        size={200}
+        />
+      </div>
+      <div className="two-thirds column">
+        <h5>Email</h5>
+        <p>{email}</p>
+        <h5>Something Else</h5>
+        <p>Something Else</p>
+        <h5>Something Else</h5>
+        <p>Something Else</p>
+      </div>
       </CardText>
     </Card>
   )
 }
 
 export default UserProfile;
+
