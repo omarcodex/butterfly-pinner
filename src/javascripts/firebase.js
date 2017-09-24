f7084c16405300e440722546ec1e66db96815a8b
omarcodex <omaralimalik@gmail.com>
2017-09-24 15:08:51 -0700

    Save user data to cloud upon login

--- src/javascripts/firebase.js
+++ src/javascripts/firebase.js
@@ -1,7 +1,7 @@
 import * as firebase from 'firebase';
 
-import { testAction } from "../actions/userActions";
-import store from "../store/configureStore";
+import { testAction } from '../actions/userActions';
+import store from '../store/configureStore';
 
 let config = {
   apiKey: 'AIzaSyBhSdMEX0P-QGwfZLOYLApr63rmERuxb_o',
@@ -14,17 +14,49 @@ let config = {
 
 firebase.initializeApp(config);
 
-firebase.auth().onAuthStateChanged(firebaseUser=>{
+var currentUser = null;
+var currentUserID = null;
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
   if (firebaseUser) {
-    // console.log(firebaseUser);
+    console.log('User signed in: ', JSON.stringify(firebaseUser));
+    console.log(JSON.stringify(firebaseUser.uid));
+    writeUserData(firebaseUser);
+    // localStorage.removeItem(firebaseAuthKey);
+
+    // // here you could authenticate with you web server to get the
+    // // application specific token so that you do not have to
+    // // authenticate with firebase every time a user logs in
+    // localStorage.setItem(appTokenKey, user.uid);
+    //
+    // // store the token
+    // this.props.history.push('/app/home');
   } else {
-    console.log("Not logged in!");
+    console.log('Not logged in!');
   }
 });
 
-export default firebase;
+let unsubscribe = store.subscribe(
+  () =>
+    // console.log(store.getState())
+    2 + 2
+);
 
-let unsubscribe = store.subscribe(() =>
-  // console.log(store.getState())
-  2+2
-)
+function writeUserData(user) {
+  var appUsersRef = firebase.database().ref('/app_users');
+  var appUserRef = appUsersRef.child(user.uid);
+  appUserRef.once('value').then(function(snapshot) {
+    // default doWhop writes first, so this check resulted in
+    // userData not being written changed set to update
+    var userData = {
+      displayName: user.displayName,
+      photoURL: user.photoURL ? user.photoURL : 'NA',
+      uid: user.uid,
+      email: user.email,
+      lastLoginDatePST: Date()
+    };
+    appUserRef.update(userData);
+  });
+}
+
+export default firebase;
