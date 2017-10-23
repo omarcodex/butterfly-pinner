83ad95ab3dd264686c0812313438a3f37165a63b
omarcodex <omaralimalik@gmail.com>
2017-10-23 13:42:49 -0700

    Add basic sighting card styling

--- src/components/SightingList.jsx
+++ src/components/SightingList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Card, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import { Card, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 // import styled from 'styled-components';
 // const Img = styled.img`
 //   margin:
@@ -11,28 +11,20 @@ const SightingList = props => {
     <div>
       <h1>Sighting List</h1>
       {props.sightings.map(s => (
-        <ul>
-          <li>{s.scientificName}</li>
-          <li>
-            Count:
-            {s.count}
-          </li>
-          <li>
-            Sex:
-            {s.sex}
-          </li>
-          <li>
-            Lat:
-            {s.lat}
-          </li>
-          <li>
-            Long:
-            {s.lon}
-          </li>
-          <li>
-            <img src={s.photoURL} alt={s.scientificName} width="300px" />
-          </li>
-        </ul>
+        <Card>
+          <CardTitle title={s.scientificName} subtitle={'Lat: ' + s.lat + ', Long: ' + s.lon} />
+          <CardMedia>
+            <img
+              src={s.photoURL || 'https://evolution.berkeley.edu/evolibrary/images/evo/ontogenew.gif'}
+              alt={s.scientificName}
+            />
+          </CardMedia>
+          <CardText>
+            <h3>Count: {s.count}</h3>
+            <h3>Sex: {s.sex}</h3>
+            <h3>Location: {s.lat + ', ' + s.lon}</h3>
+          </CardText>
+        </Card>
       ))}
     </div>
   );
